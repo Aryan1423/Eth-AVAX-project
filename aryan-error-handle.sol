0b3b205b8d75bdaec0a760cd2caa795c58b38799
Aryan Singh <113651343+Aryan1423@users.noreply.github.com>
2024-06-09 16:03:18 +0530

    Update aryan-error-handle.sol

--- aryan-error-handle.sol
+++ aryan-error-handle.sol
@@ -1,15 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity ^0.8.18;
 
-contract aryan {
-    function errorhandle(uint _value) public pure returns (bool) {
+contract AryanBank {
 
-        require(_value > 5, "Greater than 5");
+    mapping(address => uint) private balances;
+    event Deposit(address indexed account, uint amount);
+    event Withdrawal(address indexed account, uint amount);
 
-        uint newvalue = _value + 5;
-        assert(newvalue > _value);
+    function deposit() external payable {
+        if (msg.value==0) {
+            revert("Deposit amount must be greater than 0");
+        }
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);}
 
-        if (_value == 11) {
-            revert("It cannot be 10");}
-        return true;
+    function withdraw(uint amount) external {
+        require(amount > 0, "Withdraw amount must be greater than zero");
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        assert(amount < 2000);
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+        emit Withdrawal(msg.sender, amount);}
+
+    function getBalance(address account) external view returns (uint) {
+        return balances[account];
     }}
